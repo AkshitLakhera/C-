38aae934203edf05e3968690eb5c1d4f69e8b68f
“[Akshit <akshitlakhera14@gmail.com>
2024-05-21 22:20:11 +0530

    learned loops switch statements in cpp

--- demo.cpp
+++ demo.cpp
@@ -115,38 +115,115 @@ Ask user to  enter marks and print follwoig  message
 /* Take the day and no andprint the corresponding day
 for 1 print Monday
 for 2 prinnt Tuesday and so on 7 print Sunday */
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int day;
+//     cin >> day;
+//     switch (day)
+//     {
+//         // case will acc to argument in switch
+//     case 1:
+//         cout << "Monday";
+//         break;
+//     case 2:
+//         cout << "Tuesday";
+//         break;
+//     case 3:
+//         cout << "Wednesday";
+//         break;
+//     case 4:
+//         cout << "Thursday";
+//         break;
+//     case 5:
+//         cout << "Friday";
+//         break;
+//     case 6:
+//         cout << "Saturday";
+//         break;
+//     case 7:
+//         cout << "Sunday";
+//         break;
+//     default:
+//         cout << "Invalid";
+//     }
+//     cout << "Done";
+// }
+
+//    Array  basics
+//  1d array
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int arr[5]; // making array of size 5
+//     cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+//     arr[4] += 10; // we can do any commutation
+//     cout << arr[4];
+//     return 0;
+// }
+// Where is entire error in stored ?
+// The entire array is stored in specific address called memorey address .0th address doesn't know where will be stored, but rest  of will be store consecutively( one after another)
+
+//   2d array
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     // 2d array
+//     int arr[3][5];
+//     arr[1][3] = 48;
+//     cout << arr[1][2];
+//     return 0;
+// }
+
+//  String
+// It is collection of characters eg 'z'
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     string s = "Striver";
+//     int len = s.size();
+//     s[len - 1] = 'z'; //"z" will not work
+//     cout << s[len - 1];
+//     return 0;
+// }
+
+// For loops
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-    int day;
-    cin >> day;
-    switch (day)
-    {
-        // case will acc to argument in switch
-    case 1:
-        cout << "Monday";
-        break;
-    case 2:
-        cout << "Tuesday";
-        break;
-    case 3:
-        cout << "Wednesday";
-        break;
-    case 4:
-        cout << "Thursday";
-        break;
-    case 5:
-        cout << "Friday";
-        break;
-    case 6:
-        cout << "Saturday";
-        break;
-    case 7:
-        cout << "Sunday";
-        break;
-    default:
-        cout << "Invalid";
-    }
-    cout << "Done";
-}
+// int main()
+// {
+//     for (int i = 0; i < 5; i++)
+//     {
+
+//         cout << "Hey, I am akshit lakhera" << endl;
+//     }
+// }
+// converting in while loop
+// int main()
+// {
+//     int i = 1;
+//     while (i < 5)
+//     {
+//         cout << "Hey, I am akshit lakhera" << endl;
+//         i = i + 1;
+//     }
+// }
+
+// Do while loop concept
+// int main()
+// {
+//     int i = 2;
+//     do
+//     {
+//         cout << "striver" << i << endl;
+//         i = i + 1;
+//     } while (i <= 1);
+//     cout << i << endl;
+//     return 0;
+// }
+
+//     Functions
